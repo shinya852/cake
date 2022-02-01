f880fa9ba91f9704d197682ceb12c085cf58ab74
shinya852 <shinya.june@gmail.com>
2022-02-01 14:44:55 +0000

    next debug admin genre controller

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,5 +1,6 @@
 class Admin::GenresController < ApplicationController
 
+　#beforeアクション追加予定
   #今回はジャンルページの一覧に新規登録機能の追加を行う
   def index
     @genre = Genre.new(genre_params)
@@ -11,8 +12,18 @@ class Admin::GenresController < ApplicationController
     @genre.save
     #以下保存の有無を判定する条件式の追加
   end
-
-  #update追加予定
+  
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    #条件式追加予定
+  end
+  
+  
   private
 
   def genre_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   #顧客用
   devise_for :customers
   #管理者用
-  devise_for :admin
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
   namespace :admin do
-    resources :genres
+  resources :genres
   end
 end
