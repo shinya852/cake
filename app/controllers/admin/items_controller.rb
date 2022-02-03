cc8e4c010bb06ae7d7b097430668ed856a27db3b
shinya852 <shinya.june@gmail.com>
2022-02-03 13:25:50 +0000

    create items model & addmin/items comtroller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,20 @@
 class Admin::ItemsController < ApplicationController
+#ceforeアクション追加予定
+  def index
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def new
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
 end
